f9116ce6ac6b4069a40e09b3b72d6656c4d80dea
genteanaranea <50955782+gentearanea@users.noreply.github.com>
2020-07-03 01:16:53 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import "./styles.css";
+
+const fill = document.querySelector(".fill");
+const empties = document.querySelectorAll(".empty");
+
+fill.addEventListener("dragstart", dragStart);
+fill.addEventListener("draged", dragEnd);
+
+for (const empty of empties) {
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleave", dragLeave);
+  empty.addEventListener("drop", dragDrop);
+}
+
+function dragStart() {
+  this.className += " hold";
+  setTimeout(() => (this.className = "invisible"), 0);
+}
+
+function dragEnd() {
+  this.className = "fill";
+}
+
+function dragOver(e) {
+  // console.log("over");
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  // console.log("enter");
+  e.preventDefault();
+  this.className += " hovered";
+}
+
+function dragLeave() {
+  // console.log("leave");
+  this.className = "empty";
+}
+
+function dragDrop() {
+  // console.log("drop");
+  this.className = "empty";
+  this.append(fill);
+}
